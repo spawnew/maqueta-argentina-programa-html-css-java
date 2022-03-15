f3c49cf63f7a9807be9576872dab511818d6549a
spawnew <96084099+spawnew@users.noreply.github.com>
2022-03-15 00:42:27 -0300

    proyectomaqueta
    css java y html aun no esta terminado la parte java

---
+++ java.js
@@ -0,0 +1,38 @@
+
+function cambiar()
+{
+    document.getElementById("edit").style.display="block";
+let texto = document.getElementById("abajo").innertext;
+console.log(texto);
+
+};
+function mifuncion2(valor){
+    document.getElementById("abajo").innerText=valor;
+    
+    };
+    
+
+
+
+function logMessage(message){
+
+
+    console.log(message+"<br>");
+    }
+//controlo si se presiona el enter
+let textarea=document.getElementById("edit")
+textarea.addEventListener('keyup', (e) => {
+    logMessage('key "${e.key}" released [event: keyup]' );
+    if(e.key=="Enter"){
+        document.getElementById("edit").style.display="none"
+        
+    }
+});
+
+
+
+
+
+
+
+
